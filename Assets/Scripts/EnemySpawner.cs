84c0e23b4ec1982e6b35fa6b85aa71d4a4d540fa
yaqinhpro <yaqinhpro@outlook.com>
2020-08-08 17:07:50 +0800

    Add Enemy Spawner and logic

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public List<Transform> targetSpawnPoints;
+    public int spawnNumberPerRound = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0; i < targetSpawnPoints.Count; i++)
+        {
+            for(int j=0; j < spawnNumberPerRound; j++)
+            {
+                Instantiate(enemyPrefab, targetSpawnPoints[i].position, Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
