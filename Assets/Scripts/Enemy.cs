a51fec09574366679668747f5709ecabfa99285e
yaqinhpro <yaqinhpro@outlook.com>
2020-08-09 12:58:25 +0800

    Add Sleep after enemy died

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -16,6 +16,8 @@ public class Enemy : MonoBehaviour
     private BoxCollider2D bc;
     private Animator anim;
 
+    private float waitCounter = 0;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -54,16 +56,38 @@ public class Enemy : MonoBehaviour
 
             if (health <= 0)
             {
-                isDead = true;
-                rb.isKinematic = true;
-                rb.velocity = new Vector2(0, 0);
-                bc.enabled = false;
-                GameObject deathEffect = Instantiate(deathEffectPrefab, transform.position, Quaternion.identity);
-                Destroy(deathEffect, 0.5f);
-
-                anim.SetBool("isMoving", false);
-                anim.Play(hitterDirection > 0? "RightFallDeath" : "LeftFallDeath");
+                Die(hitterDirection);
             }
         }
+    }
+
+    private void Die(int hitterDirection)
+    {
+        isDead = true;
+        rb.isKinematic = true;
+        rb.velocity = new Vector2(0, 0);
+        bc.enabled = false;
+        GameObject deathEffect = Instantiate(deathEffectPrefab, transform.position, Quaternion.identity);
+        Destroy(deathEffect, 0.5f);
+
+        anim.SetBool("isMoving", false);
+        anim.Play(hitterDirection > 0 ? "RightFallDeath" : "LeftFallDeath");
+
+        StartCoroutine(DieInPeace());
+    }
+
+    IEnumerator DieInPeace()
+    {
+        Time.timeScale = 0;
+        waitCounter = 0;
+        while (waitCounter < 0.05f)
+        {
+            waitCounter += Time.unscaledDeltaTime;
+
+            //Yield until the next frame
+            yield return null;
+        }
+
+        Time.timeScale = 1;
     }
 }
